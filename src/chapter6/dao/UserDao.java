874b2a63da8537a5001b5e607c94ea6c174c7507
goto_riko <goto.riko@alhinc.jp>
2026-05-20 15:44:30 +0900

    簡易Twitterの実践課題その①_修正 ■UserService ・インデントをそろえる ・isEmpty→isBlankに修正 ■UserDao ・isEmpty→isBlankに修正 ・余計なセミコロンを削除

--- src/chapter6/dao/UserDao.java
+++ src/chapter6/dao/UserDao.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.exception.NoRowsUpdatedRuntimeException;
 import chapter6.exception.SQLRuntimeException;
@@ -183,7 +185,7 @@ public class UserDao {
 			sql.append("    name = ?, ");
 			sql.append("    email = ?, ");
 			//パスワードを受け取った場合
-			if (!user.getPassword().isEmpty()) {
+			if (!StringUtils.isBlank(user.getPassword())) {
 				sql.append("    password = ?, ");
 			}
 			sql.append("    description = ?, ");
@@ -195,14 +197,14 @@ public class UserDao {
 			ps.setString(1, user.getAccount());
 			ps.setString(2, user.getName());
 			ps.setString(3, user.getEmail());
-			if (!user.getPassword().isEmpty()) {
+			if (!StringUtils.isBlank(user.getPassword())) {
 				ps.setString(4, user.getPassword());
 				ps.setString(5, user.getDescription());
 				ps.setInt(6, user.getId());
 			}else{
 				ps.setString(4, user.getDescription());
 				ps.setInt(5, user.getId());
-			};
+			}
 
 			int count = ps.executeUpdate();
 			if (count == 0) {
--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -119,11 +121,11 @@ public class UserService {
 	    Connection connection = null;
 	    try {
 	    	//パスワードが入力されている場合のみ暗号化する
-	    	if (!user.getPassword().isEmpty()) {
-	        // パスワード暗号化
-	        String encPassword = CipherUtil.encrypt(user.getPassword());
-	        user.setPassword(encPassword);
-	    	}
+			if (!StringUtils.isBlank(user.getPassword())) {
+				// パスワード暗号化
+				String encPassword = CipherUtil.encrypt(user.getPassword());
+				user.setPassword(encPassword);
+			}
 
 	        connection = getConnection();
 	        new UserDao().update(connection, user);
