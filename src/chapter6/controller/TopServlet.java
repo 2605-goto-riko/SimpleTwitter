d263a3f366048b7cf0b67e5aaad1c17cdaa4fdff
goto_riko <goto.riko@alhinc.jp>
2026-05-26 15:26:47 +0900

    【新規追加】簡易Twitter仕様追加課題(フィルター)

--- src/chapter6/controller/TopServlet.java
+++ src/chapter6/controller/TopServlet.java
@@ -11,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import chapter6.beans.User;
+import chapter6.beans.UserComment;
 import chapter6.beans.UserMessage;
 import chapter6.logging.InitApplication;
+import chapter6.service.CommentService;
 import chapter6.service.MessageService;
 
 @WebServlet(urlPatterns = { "/index.jsp" })
@@ -46,8 +48,11 @@ public class TopServlet extends HttpServlet {
 		/*userIdをServiceに渡す*/
 		String userId = request.getParameter("user_id");
 		List<UserMessage> messages = new MessageService().select(userId);
+		String messageId = request.getParameter("message_id");
+		List<UserComment> comments = new CommentService().select(messageId);
 
 		request.setAttribute("messages", messages);
+		request.setAttribute("comments", comments);
 		request.setAttribute("isShowMessageForm", isShowMessageForm);
 		request.getRequestDispatcher("/top.jsp").forward(request, response);
 	}
---
+++ src/chapter6/filter/LoginFilter.java
@@ -0,0 +1,50 @@
+package chapter6.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import chapter6.beans.User;
+
+@WebFilter(urlPatterns = {"/setting", "/edit" })
+public class LoginFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		/*型変換
+		 *ServletRequest→HttpRequest */
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+		//セッション領域のログイン情報を取得
+		User user = (User) httpRequest.getSession().getAttribute("loginUser");
+
+		/*①セッション領域にログイン情報が存在する(ログインしている)
+			→サーブレットを実行
+			②ログインしていない
+			→ログインページに遷移、エラーメッセージを出力*/
+			if(user != null) {
+				chain.doFilter(request, response); // サーブレットを実行
+			}else {
+				httpRequest.setAttribute("errorMessages", "ログインしてください");
+				httpRequest.getRequestDispatcher("login.jsp").forward(request, response);
+			}
+	}
+
+	@Override
+	public void init(FilterConfig config) {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
